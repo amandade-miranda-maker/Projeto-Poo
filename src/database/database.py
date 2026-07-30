67601b1a0c2357ebdda606f830ee26b18909ea96
DOUGLAS DE SOUZA PAIXÃO <douglas_paixao@estudante.sesisenai.org.br>
2026-07-30 20:47:07 -0300

    refactor: converte conexão com banco para classe Database

--- src/database/database.py
+++ src/database/database.py
@@ -4,11 +4,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def conectar():
-    return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT")),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-    )
+
+class Database:
+
+    def __init__(self):
+        self._host = os.getenv("DB_HOST")
+        self._port = int(os.getenv("DB_PORT"))
+        self._user = os.getenv("DB_USER")
+        self._password = os.getenv("DB_PASSWORD")
+        self._database = os.getenv("DB_NAME")
+
+    def conectar(self):
+        try:
+            return mysql.connector.connect(
+                host=self._host,
+                port=self._port,
+                user=self._user,
+                password=self._password,
+                database=self._database,
+            )
+        except mysql.connector.Error as erro:
+            print(f"Erro ao conectar ao banco: {erro}")
+            return None
